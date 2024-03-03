ba1230c71354c82de07566e374902a6de650f69d
Dima <dimadeg2202@gmail.com>
2024-03-03 16:46:58 +0500

    Ui input to enter data

--- src/components/ui/inputData/InputData.tsx
+++ src/components/ui/inputData/InputData.tsx
@@ -1,20 +1,27 @@
-
+import { FC } from 'react'
+import { InputData } from 'types/types.data'
 
 import s from './InputData.module.scss'
 
-interface Props {
-  type: string
-  placeholder: string
-  
-}
+import { CiHashtag, CiLock, CiMail } from "react-icons/ci";
 
 
-const InputData = ({}) => {
-
-
-  return (
-    <div>InputData</div>
-  )
+const InputData: FC<InputData> = ({
+	error,
+	type,
+	register,
+	name,
+	...rest
+}) => {
+	return (
+		<div className={s.container}>
+			<div className={s.inputCont}>
+				{name === 'username' ? <CiMail/>  : name === 'confirmEmail' ? <CiHashtag />  :  <CiLock />}
+				<input type={type} {...register(name)} {...rest}  />
+			</div>
+			{!!error && <div className='error'>{error}</div>}
+		</div>
+	)
 }
 
 export default InputData
