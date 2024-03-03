867233847aef375e0e4665679780c0656ed9663f
Dima <dimadeg2202@gmail.com>
2024-03-03 16:45:41 +0500

    Hook to make request to server

---
+++ src/components/screens/signUp/useSignUp.ts
@@ -0,0 +1,91 @@
+import { yupResolver } from '@hookform/resolvers/yup'
+import { useMutation } from '@tanstack/react-query'
+import { useCallback, useMemo, useState } from 'react'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import { signUp } from 'services/SignUp.service'
+import { FormFields, SignUpInput } from 'types/types.data'
+import * as Yup from 'yup'
+
+export const useSignUp = () => {
+	const [type, setType] = useState<string>('')
+
+	let passwordSchema = Yup.string().defined()
+	let confirmPwdSchema = Yup.string().defined()
+	let confirmEmailSchema = Yup.number().defined()
+
+	if (type === 'signup') {
+		passwordSchema = passwordSchema
+			.required('Password is required')
+			.min(5, 'Password must be at least 5 characters long')
+		confirmPwdSchema = confirmPwdSchema
+			.required('Repeat the password')
+			.oneOf([Yup.ref('password')], "Passwords don't match")
+		confirmEmailSchema = confirmEmailSchema
+			.transform((value, originalValue) => {
+				if (
+					originalValue === undefined ||
+					originalValue === null ||
+					originalValue === ''
+				) {
+					return undefined
+				}
+				return value
+			})
+			.required('Enter the verification code')
+			.typeError('Invalid code')
+	}
+
+	const formSchema = Yup.object().shape({
+		username: Yup.string()
+			.required('Email is required')
+			.email('Incorrect email format'),
+		password: passwordSchema,
+		confirmPwd: confirmPwdSchema,
+		confirmEmail: confirmEmailSchema
+	})
+
+	const {
+		register,
+		handleSubmit,
+		reset,
+		formState: { errors }
+	} = useForm<FormFields>({ resolver: yupResolver(formSchema) })
+
+	const { mutate, isPending, isSuccess, error } = useMutation({
+		mutationKey: [type],
+		mutationFn: (data: SignUpInput) =>
+			type === 'signup' ? signUp.CreateUser(data) : signUp.SendCode(data)
+	})
+
+	const onSubmitCallback = useCallback<SubmitHandler<SignUpInput>>(
+		userData => {
+			console.log(userData)
+
+			mutate(userData)
+		},
+		[mutate]
+	)
+
+	return useMemo(
+		() => ({
+			setType,
+			onSubmit: onSubmitCallback,
+			handleSubmit,
+			errors,
+			register,
+			isPending,
+			isSuccess,
+			error
+		}),
+		[
+			setType,
+			onSubmitCallback,
+			handleSubmit,
+			errors,
+			register,
+			isPending,
+			isSuccess,
+			error
+		]
+	)
+}
